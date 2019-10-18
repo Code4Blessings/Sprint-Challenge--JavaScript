2d858e4992172b0ad27e9e6a39bd8069073776c4
Code4Blessings <robinwarden@code4blessings.com>
2019-10-18 12:07:13 -0400

    Completed function.js file

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,31 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb) {
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2) {
+  return num1 + num2;
+}
 
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +38,12 @@
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation: 
+/*
+--NestedFunction is able to access the internal varaiable because it is nested inside the
+function.As long as the variable is inside the
+function, there is access.If you place the variable outside of the
+function, then there is no access.
+ */
 
 
 const external = "I'm outside the function";
